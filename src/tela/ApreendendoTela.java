ba2d01931db39f39f86a878fb65f4dbb5652ed02
thuani.bittencourtt <thuani.bittencourtt@catolicasc.org.br>
2017-11-13 16:27:51 -0200

    Primeira versão do projeto. Trabalho da Disciplina de OO da Católica de SC - Professor Glauco Vinicius Scheffel.

---
+++ src/tela/ApreendendoTela.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tela;
+
+
+import Telas.TelaMenuAluno;
+
+/**
+ *
+ * @author thuani.bittencourtt
+ */
+public class ApreendendoTela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new TelaMenuAluno().setVisible(true);
+      
+    }
+    
+}
